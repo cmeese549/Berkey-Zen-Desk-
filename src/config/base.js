6dfb9eea036135d9de2cb084808c492713591b35
JEITech <chris@meesewebdesigns.com>
2018-12-18 12:58:37 -0700

    Initial commit

--- src/config/base.js
+++ src/config/base.js
@@ -2,7 +2,7 @@
 
 // Settings configured here will be merged into the final config object.
 export default {
-	ACCOUNT_KEY: '',
+	ACCOUNT_KEY: 'YzXWHZv4IzDHt5R1JmNR5X0B7MbkKqxr',
 	// Set to 'docked' or 'normal' for docked or normal mode respectively
 	THEME: 'normal'
 }
