b4a066eec02c54a0c1026e484067f915c58a69d3
JEITech <chris@meesewebdesigns.com>
2019-01-08 15:05:14 -0700

    Wow so much progress today wow, now it actually talks to lex

---
+++ assets/scripts/bot.js
@@ -0,0 +1,310 @@
+const botui = new BotUI('my-botui-app');
+const user = {};
+user.first_name = 'Name';
+
+async function Run(){
+    Init();
+}
+
+async function Init() {
+    await GetName();
+    InitialGreeting();
+}
+
+async function InitialGreeting(){
+    await SendMessage(('Hello ' + user.first_name + ', Welcome to Berkey Filters!'), 0);
+    await SendMessage('Is there anything I can help you with today?', 0);
+    await botui.action.button({
+        action: [
+          {
+            text: 'Yes',
+            value: true
+          },
+          {
+              text: 'No',
+              value: false
+          }
+        ]
+    }).then(function (res){
+        if(res.value){
+            InitialHelp();
+        }else {
+            botui.message.add({
+                delay: 100,
+                content: 'Alright!  If you need anything in the future, I\'ll be right here.'
+            }).then( () => {
+                Prompt();
+            });
+        }
+    });
+   
+}
+
+async function GetName(){
+    return new Promise(async (resolve, reject) => {
+        let yourName = '';
+        let name = 'name=';
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                yourName = (c.substring(name.length, c.length));
+            }
+        }
+        if(yourName != ''){
+            user.first_name = yourName;
+            resolve();
+        }else {
+            await SetName();
+            resolve();
+        }
+    });
+}
+
+async function SetName(){
+    return new Promise(async (resolve, reject) => {
+        await SendMessage('Hello!  What is your name?');
+        botui.action.text({
+            action: {
+                placeholder: 'Your name'
+        }
+        }).then((res) => {
+            user.first_name = res.value;
+            let str = 'name=' + res.value;
+            document.cookie = str;
+            resolve();
+        });
+    });
+}
+
+async function InitialHelp(){
+    await botui.message.add({
+        delay: 500,
+        content: 'What do you need help with today?'
+    });
+    await botui.action.button({
+        action : [
+            {
+                text: 'Order Issue',
+                value: 'orderIssue'
+            },
+            {
+                text: 'Product Issue',
+                value: 'productIssue'
+            },
+            {
+                text: 'Other',
+                value: 'other'
+            }
+        ]
+    }).then(function (res){
+        switch(res.value){
+            case 'orderIssue':
+                OrderIssue();
+                break;
+            case 'productIssue':
+                ProductIssue();
+                break;
+            case 'other':
+                OtherIssue();
+                break;
+            default:
+                break;
+        }
+    });
+}
+
+async function OrderIssue() {
+
+}
+
+async function ProductIssue() {
+
+}
+
+async function OtherIssue() {
+    await botui.message.add({
+        delay: 500,
+        content: 'Alright!  What do you need to know?'
+    });
+    await Prompt();
+}
+
+async function LexResponse(lexData){
+    lexData = JSON.parse(lexData);
+                if (lexData.multipleFound) {
+                    const intents = [];
+                    for (let i = 0; i < lexData.multiple.length; i++) {
+                        let qr = [];
+                        qr.push(lexData.multiple[i].intentName);
+                        qr.push(lexData.multiple[i].intentName);
+                        intents.push(qr);
+                    }
+                    let qrData = await SendQuickReplies(intents, 'I detected multiple questions!  I can only handle one at a time.  Were you asking about one of these topics?', 0);
+                    await botui.action.button({
+                        action : qrData
+                    });
+                } else {
+                    if (lexData.intentName == null) {
+                        //No intent has been found, ask the user to rephrase their message
+                        const greet = "Hello, " + user.first_name + "!";
+                        await SendMessage(greet, 0)
+                        await SendMessage('I\'m sorry, I wasn\'t quite able to understand you.  I\'ve made a note of this so someone can help teach me how to respond!', 1000);
+                        await SendMessage('If I haven\'t been very helpful, please give us a call at 1-800-350-4170', 1000);
+                        await Prompt('Ask another question');
+                    } else {
+                        //Check if there are multiple messages to send, or just one
+                        if (typeof lexData.message.messages !== 'undefined') {
+                            //Send array of messages to user in proper order
+                            await SendMessages(lexData.message.messages, 0);
+                            await FollowUp(lexData.intentName);
+                            //let analytics = new dashData(messageText, senderID, event);
+                            //analytics.intent = { "name": lexData.intentName };
+                            //dashbot.logOutgoing(analytics);
+                        } else if (typeof lexData.message !== 'undefined'){
+                            //Send the single message to the user
+                            await SendMessage(lexData.message, 0);
+                            await FollowUp(lexData.intentName);
+                        }
+                    }
+                }
+}
+
+async function GetGif(str){
+    return new Promise(async (resolve, reject) => {
+        $.ajax({
+            type: 'POST',
+            url: '/gif',
+            data: 
+            {
+               keyword: str
+            }, 
+            success: async function(res, status){
+               resolve(res);
+            },
+            error:   function(jqXHR, textStatus, errorThrown) {
+                console.log("Error, status = " + textStatus + ", " +
+                      "error thrown: " + errorThrown
+                );
+            }
+        });
+    });
+}
+
+//Helper functions
+async function Prompt(place){ 
+    if(typeof place == 'undefined'){
+        place = 'Ask me a question!';
+    }
+    return new Promise(async (resolve, reject) => {
+        await botui.action.text({
+            action: {
+                placeholder: place
+            }
+        }).then(async function(res) {
+            $.ajax({
+                type: 'POST',
+                url: '/lexify',
+                data: 
+                {
+                    msg: res.value,
+                    name: 'name'
+                }, 
+                success: async function(lexData, status){
+                    await LexResponse(lexData);
+                },
+                error:   function(jqXHR, textStatus, errorThrown) {
+                    console.log("Error, status = " + textStatus + ", " +
+                          "error thrown: " + errorThrown
+                    );
+                }
+            });
+        });
+    }); 
+}
+
+async function SendMessage(msg, delay){
+    return new Promise(async (resolve, reject) => {
+        await botui.message.add({
+            delay: delay,
+            content: msg
+        });
+        resolve();
+    });
+}
+
+async function SendMessages(msgs, delay){
+    return new Promise(async (resolve, reject) => {
+        msgs.forEach(async(elem) => {
+            await SendMessage(elem.value, 0);
+        });
+        resolve();
+    });
+}
+
+async function SendQuickReplies(buttons, msg, delay){
+    return new Promise(async (resolve, reject) => {
+        await SendMessage(msg, delay);
+        let buttonData = [];
+        buttons.forEach((elem) => {
+            let tempData = 
+            {
+                text: elem[0],
+                value: elem[1]
+            }
+            buttonData.push(tempData);
+        });
+        resolve(buttonData);
+    });
+}
+
+
+async function FollowUp(intent){
+    console.log(intent);
+    switch (intent) {
+        case 'Hi':
+            let jz = await GetGif('Hello');
+            jz = (JSON.parse(jz).data.images.fixed_height.url);
+            let str = '![cool gif](' + jz + ')';
+            console.log(str);
+            //await SendMessage(str, 0);
+            Prompt();
+            break;
+        case 'Bye':
+        case 'Joke':
+        case 'Humans':
+        case 'Thanks':
+        case 'HowAreYou':
+            Prompt();
+            break;
+        default:
+            let qrData = await SendQuickReplies([
+                ['This helped, thanks!', 'Thanks'],
+                ["This didn't help.", 'I need a human']
+                ], 'Was I able to help?', 0);
+                await botui.action.button({ action: qrData}).then(async (res) => {
+                    UserFeedback(res.value);
+                });
+            break;
+    }
+}
+
+async function UserFeedback(msg)
+{
+    switch (msg)
+    {
+        case 'Thanks':
+            await SendMessage('Glad I could help!', 0);
+            await Prompt('Ask another question');
+            break;
+        default:
+            await SendMessage('Sorry I couldn\'t help out more!');
+            await Prompt('Ask another question');
+    }
+}
+
+Run();
--- server.js
+++ server.js
@@ -1,12 +1,110 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const AWS = require('aws-sdk');
+const giphy = require('giphy-api')('XjJamx628YYbeFseYigMtx7UAV1XZUq3');
+const bodyParser = require('body-parser');
+let cookies;
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 const port = 3000;
 
-app.use(express.static('src'));
+app.use(express.static('assets'));
+app.use(express.static('node_modules'));
 
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname + '/index.html'));
-})
+});
 
-app.listen(port, () => console.log('Heeeeeeeeeeeeeeeeeeeeeek'));
+app.post('/gif', async function(req,res) {   
+    giphy.translate({
+        rating: 'y',
+        fmt: 'json',
+        s: req.body.keyword,
+        limit: 1
+        }, function(err, resp) {
+            res.end(JSON.stringify(resp));
+        }
+    );
+});
+
+app.post('/lexify', async function(req,res){
+    messageText = req.body.msg;
+    senderID = req.body.name;
+        function regx(str) {
+            return JSON.parse(JSON.stringify(str).replace(/\\"/g, '"').replace(/\"{/g, '{').replace(/\"}]}"/g, '"}]}'));
+        }
+        function send(params) {
+            return new Promise((resolve, reject) => {
+                lexruntime.postText(params, function(err, data) {
+                    if (err) {
+                        resolve(err);
+                    } else {
+                        data = regx(data);
+                        resolve(data);
+                    }
+                });
+            });
+        };
+        const userInput = messageText;
+        AWS.config.region = 'us-west-2';
+        const lexruntime = new AWS.LexRuntime();
+        const params = {
+            botAlias: "BerkeyBot",
+            botName: "BerkeyBot",
+            inputText: messageText,
+            userId: senderID,
+            sessionAttributes: {}
+        };
+        const init = await send(params);
+        if (init.intentName == null) {
+            const messageStrings = userInput.split(' ');
+            const intents = [];
+            let logStr = 'Log info! :';
+
+            function dupes(arr, lexer) {
+                for (let j = 0; j < arr.length; j++) {
+                    if (arr[j].intentName == lexer.intentName) {
+                        return false;
+                    }
+                }
+                return true;
+            };
+            for (let i = 0; i < messageStrings.length; i++) {
+                logStr += 'Iteration ' + i + ' ---- ';
+                if (messageStrings[i] !== '') {
+                    params.inputText = messageStrings[i];
+                    logStr += ' params updated ' + JSON.stringify(params);
+                    let lexer = await send(params);
+                    logStr += 'Posting to lex now ---- ';
+                    logStr += JSON.stringify(lexer);
+                    let checkDupes = dupes(intents, lexer);
+                    if (checkDupes && typeof lexer.intentName == 'string' && lexer.message !== 'none' && lexer.intentName !== 'Bye' && lexer.intentName !== 'Sorry' && lexer.intentName !== 'Initialize' && lexer.intentName !== 'GoAway' && lexer.intentName !== 'Thanks' && lexer.intentName !== 'Hi' && lexer.intentName !== 'Help' && lexer.intentName !== 'Insult') {
+                        logStr += 'Should push to intents now';
+                        intents.push(lexer);
+                    }
+                    logStr += 'End of log for iteration ' + i + '.'
+                }
+            }
+            if (intents.length > 1) {
+                init.multipleFound = true;
+                init.multiple = intents;
+                res.end(JSON.stringify(init));
+            } else if (intents.length == 1) {
+                intents[0].multipleFound = false;
+                res.end(JSON.stringify(init));
+            } else if (intents.length == 0) {
+                init.multipleFound = false;
+                res.end(JSON.stringify(init));
+            }
+        } else {
+            init.multipleFound = false;
+            res.end(JSON.stringify(init));
+        }
+});
+
+app.listen(port, () => {
+    console.log('Heeeeeeeeeeeeeeeeeeeeeek');
+});
